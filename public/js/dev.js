ce43d7af19a6346712122a0df6ba6d28b8b3c073
danieluy <danielsosa.dev@gmail.com>
2016-11-28 17:59:17 -0300

    Working on DS.dev

--- public/js/dev.js
+++ public/js/dev.js
@@ -20,7 +20,6 @@ var $dev = {
       url: 'http://' + window.location.host + '/lang',
       successCb: (function(data){
         this.data = JSON.parse(data);
-        console.log(this.data);
         this.render();
       }).bind(this),
       errorCb: function(err){
@@ -35,5 +34,28 @@ var $dev = {
   render: function(){
     this.nav_links.innerHTML = Mustache.to_html(this.nav_links_template, this.data.nav);
     this.footer.innerHTML = Mustache.to_html(this.footer_template, this.data.footer);
+    $dev_nav.init();
+  }
+}
+
+var $dev_nav = {
+  nav_links: [],
+  init: function(){
+    this.domCache();
+  },
+  domCache: function(){
+    var nav_links_aux = document.getElementsByClassName('nav-link');
+    for (var i = 0; i < nav_links_aux.length; i++) {
+      this.nav_links.push(nav_links_aux[i])
+    }
+    this.eventListeners();
+  },
+  eventListeners: function(){
+    for (var i = 0; i < this.nav_links.length; i++) {
+      this.nav_links[i].addEventListener('click', this.navigateTo.bind(this))
+    }
+  },
+  navigateTo: function(e){
+    console.log(e.target.getAttribute('href').slice(4));
   }
 }
