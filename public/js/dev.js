b93a9b692f7c0570fe5c4ee08253898164419afc
danieluy <danielsosa.dev@gmail.com>
2016-12-02 17:59:30 -0300

    Working on higlighting the links

--- public/js/dev.js
+++ public/js/dev.js
@@ -10,33 +10,72 @@ document.addEventListener("DOMContentLoaded", function() {
 var $dev = {
   page_templates: {},
   init: function(){
-    $events.on('navigateTo', this.render.subpage.bind(this));
+    $events.on('navigateToSubpage', this.render.subpage.bind(this));
     this.domCache();
   },
   domCache: function(){
-
     // Automate this cache /////////////////////////////////////////////////////
     this.page_templates.work = document.getElementById('#work').innerHTML;
     this.page_templates.academic = document.getElementById('#academic').innerHTML;
     this.page_templates.contact = document.getElementById('#contact').innerHTML;
     ////////////////////////////////////////////////////////////////////////////
-
-    this.nav_links_template = document.getElementById('dev-nav-links-template').innerHTML;
-    this.nav_links = document.getElementById('dev-nav-links');
     this.page_content = document.getElementById('page-content');
-    this.render.nav_links.call(this);
   },
   render: {
-    nav_links: function(){
-      this.nav_links.innerHTML = Mustache.to_html(this.nav_links_template, $data.getData.nav);
-      $events.emit('navLinksReady');
-    },
     subpage: function(hash){
-      console.log('subpage');
-      console.log('hash', hash);
-      console.log('this.page_templates[hash.slice(1)]', this.page_templates[hash.slice(1)]);
-      console.log('$data.getData[hash.slice(1)]', $data.getData[hash.slice(1)]);
       this.page_content.innerHTML = Mustache.to_html(this.page_templates[hash.slice(1)], $data.getData[hash.slice(1)]);
+    }
+  }
+}
+
+var $nav = {
+  li_nav_links: [],
+  init: function(){
+    this.protocol = window.location.protocol + '//';
+    this.host = window.location.host;
+    this.pathname = window.location.pathname;
+    this.hash = window.location.hash;
+    this.domCache();
+    $events.on('navigateToSubpage', this.highlightNavLink.bind(this))
+  },
+  domCache: function(){
+    this.nav_links_template = document.getElementById('dev-nav-links-template').innerHTML;
+    this.ul_nav_links = document.getElementById('dev-nav-links');
+    this.render.li_nav_links.call(this);
+  },
+  dinamicDomCache: function(){
+    var nav_links_aux = document.getElementsByClassName('nav-link');
+    for (var i = 0; i < nav_links_aux.length; i++) {
+      this.li_nav_links.push(nav_links_aux[i])
+    }
+    this.dinamicDomListeners();
+  },
+  dinamicDomListeners: function(){
+    for (var i = 0; i < this.li_nav_links.length; i++)
+      this.li_nav_links[i].addEventListener('click', this.onNavLinkClick.bind(this));
+  },
+  onNavLinkClick: function(e){
+    e.preventDefault()
+    var href = e.target.getAttribute('href');
+    history.pushState({}, "", this.protocol + this.host + href);
+    $events.emit('navigateToSubpage', href.slice(4));
+  },
+  onHashChange: function(){
+    this.hash = window.location.hash;
+    $events.emit('navigateToSubpage', this.hash);
+  },
+  highlightNavLink: function(hash){// fix this !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    for (var i = 0; i < this.li_nav_links.length; i++) {
+      if(this.li_nav_links[i].childNodes[1].getAttribute('href') === hash)
+        this.li_nav_links[i].classList.add('selected')
+      else
+        this.li_nav_links[i].classList.remove('selected')
+    }
+  },
+  render: {
+    li_nav_links: function(){
+      this.ul_nav_links.innerHTML = Mustache.to_html(this.nav_links_template, $data.getData.nav);
+      this.dinamicDomCache.call(this);
     }
   }
 }
@@ -60,60 +99,5 @@ var $data = {
         lang: lang
       }
     })
-  }
-}
-
-var $nav = {
-  page_content_templates: [],
-  nav_links: [],
-  init: function(){
-    $events.on('navLinksReady', this.domCache.bind(this));
-    this.protocol = window.location.protocol + '//';
-    this.host = window.location.host;
-    this.pathname = window.location.pathname;
-    this.hash = window.location.hash;
-    this.navigateTo();
-  },
-  domCache: function(){
-    this.page_content = document.getElementById('page-content');
-    var nav_links_aux = document.getElementsByClassName('nav-link');
-    for (var i = 0; i < nav_links_aux.length; i++) {
-      this.nav_links.push(nav_links_aux[i])
-    }
-    this.domListeners();
-  },
-  domListeners: function(){
-    for (var i = 0; i < this.nav_links.length; i++) {
-      this.nav_links[i].addEventListener('click', this.onNavLinkClick.bind(this))
-    }
-  },
-  onNavLinkClick: function(e){
-    e.preventDefault()
-    var href = e.target.getAttribute('href');
-    this.highlightNavLink(href);
-    history.pushState({}, "", this.protocol + this.host + href);
-    $events.emit('navigateTo', href.slice(4));
-  },
-  highlightNavLink: function(href){
-    for (var i = 0; i < this.nav_links.length; i++) {
-      if(this.nav_links[i].childNodes[1].getAttribute('href') === href)
-        this.nav_links[i].classList.add('selected')
-      else
-        this.nav_links[i].classList.remove('selected')
-    }
-  },
-  onHashChange: function(){
-    this.hash = window.location.hash;
-    this.navigateTo();
-  },
-  onDataUpdate: function(){
-    this.navigateTo();
-  },
-  navigateTo: function(){
-    console.log('navigateTo', this.hash, Date.now());
-    console.log('$data.getData[this.hash.slice(1)]', $data.getData[this.hash.slice(1)]);
-    console.log('$dev.page_templates.academic', $dev.page_templates.academic); // I need to find the way to await for this to be ready
-    if(this.hash !== '')
-      $events.emit('navigateTo', this.hash);
   }
 }
