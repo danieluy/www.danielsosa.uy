29eeee109658e6535b34413c0262f9477f0763bd
danieluy <danielsosa.dev@gmail.com>
2016-12-11 12:39:40 -0300

    DS - Home mobile version done

--- public/js/dev.js
+++ public/js/dev.js
@@ -29,7 +29,6 @@ var $dev = {
     this.domCache();
   },
   domCache: function(){
-
     var page_content_templates_aux = document.getElementsByClassName('page-content-template');
     for (var i = 0; i < page_content_templates_aux.length; i++) {
       this.page_templates[ page_content_templates_aux[i].id.slice(1) ] = page_content_templates_aux[i].innerHTML;
@@ -50,11 +49,13 @@ var $nav = {
   li_nav_links: [],
   init: function(){
     $events.on('domCacheReady', this.render.li_nav_links.bind(this));
+    $events.on('domCacheReady', this.domListeners.bind(this));
     $events.on('dataUpdated', this.render.li_nav_links.bind(this));
     $events.on('liNavLinksRenderReady', this.dinamicDomCache.bind(this));
     $events.on('dinamicDomCacheReady', this.dinamicDomListeners.bind(this));
     $events.on('dinamicDomCacheReady', this.onHashChange.bind(this));
     $events.on('navigateToSubpage', this.highlightNavLink.bind(this));
+    $events.on('toggleLang', this.hideMenu.bind(this));
     this.protocol = window.location.protocol + '//';
     this.host = window.location.host;
     this.pathname = window.location.pathname;
@@ -62,9 +63,19 @@ var $nav = {
     this.domCache();
   },
   domCache: function(){
+    this.navbar = document.getElementById('navbar');
+    this.menu_button = document.getElementById('menu-button');
+    this.page_content = document.getElementById('page-content');
     this.nav_links_template = document.getElementById('dev-nav-links-template').innerHTML;
     this.ul_nav_links = document.getElementById('dev-nav-links');
+    this.ul_nav_links_back = document.getElementById('dev-nav-links-back');
+
     $events.emit('domCacheReady');
+  },
+  domListeners: function(){
+    this.page_content.addEventListener('scroll', this.elevateNavbar.bind(this));
+    this.menu_button.addEventListener('click', this.displayMenu.bind(this));
+    this.ul_nav_links_back.addEventListener('click', this.hideMenu.bind(this));
   },
   dinamicDomCache: function(){
     var nav_links_aux = document.getElementsByClassName('nav-link');
@@ -77,11 +88,31 @@ var $nav = {
     for (var i = 0; i < this.li_nav_links.length; i++)
       this.li_nav_links[i].addEventListener('click', this.onNavLinkClick.bind(this));
   },
+  elevateNavbar: function(e){
+    if(e.target.scrollTop > 0)
+      this.navbar.classList.add('elevate');
+    else
+      this.navbar.classList.remove('elevate');
+  },
+  displayMenu: function(e){
+    if(window.screen.width < 960){
+      this.ul_nav_links.classList.add('slideLeft');
+      this.ul_nav_links_back.classList.add('slideLeft');
+    }
+  },
+  hideMenu: function(){
+    if(window.screen.width < 960){
+      this.ul_nav_links.classList.remove('slideLeft');
+      this.ul_nav_links_back.classList.remove('slideLeft');
+    }
+  },
   onNavLinkClick: function(e){
     e.preventDefault()
     var href = e.target.getAttribute('href');
     history.pushState({}, "", this.protocol + this.host + href);
     $events.emit('navigateToSubpage', href.slice(4));
+    if(window.screen.width < 960)
+      this.hideMenu();
   },
   onHashChange: function(){
     this.hash = window.location.hash;
@@ -123,6 +154,7 @@ var $data = {
       this.lang = 'en';
     $session.setItem('selected_lang', this.lang);
     this.updateData();
+    $events.emit('toggleLang');
   },
   initData: function(){
     console.log('initData', this.lang);
