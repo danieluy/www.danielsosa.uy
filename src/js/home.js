97a95e24284f5f8508991c644f664752ec84aaaf
danieluy <danielsosa.dev@gmail.com>
2017-03-17 17:06:26 -0300

    Height from JS and flex-box

--- src/js/home.js
+++ src/js/home.js
@@ -5,22 +5,28 @@ require("../css/home.css");
 const $events = require('./events');
 const $data = require('./data');
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   $data.init('home');
   $events.on('dataReady', $home.render.footer.bind($home));
   $home.init();
+  window.addEventListener('resize', $home.setPageContentHeight.bind($home));
 });
 
 var $home = {
-  init: function(){
+  init: function () {
     this.domCache();
   },
-  domCache: function(){
+  domCache: function () {
     this.footer_template = document.getElementById('start-footer-template').innerHTML;
     this.footer = document.getElementById('start-footer');
+    this.full_header = document.getElementById('full-header');
+  },
+  setPageContentHeight: function () {
+    this.full_header.style.height = (window.innerHeight) + 'px';
   },
   render: {
-    footer: function(){
+    footer: function () {
+      this.setPageContentHeight();
       this.footer.innerHTML = Mustache.to_html(this.footer_template, $data.getData.call($data, 'footer'));
     }
   }
