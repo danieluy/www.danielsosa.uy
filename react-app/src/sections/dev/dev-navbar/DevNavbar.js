68f4b49ade505b4697d271d4f6470794596bc9d8
danieluy <danielsosa.uy@gmail.com>
2017-03-28 19:33:13 -0300

    Working on DevNavbar responsiveness

--- react-app/src/sections/dev/dev-navbar/DevNavbar.js
+++ react-app/src/sections/dev/dev-navbar/DevNavbar.js
@@ -40,7 +40,7 @@ class DevNavbar extends Component {
             <div className="dev-nav-links">
 
               <Link to="/" className="dev-nav-mobile-back-link">
-                <svg id="dev-nav-link-icon" height="32" viewBox="0 0 24 24" width="32" xmlns="http://www.w3.org/2000/svg">
+                <svg className="dev-nav-title-icon" height="32" viewBox="0 0 24 24" width="32" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0 0h24v24H0V0z" fill="none" />
                   <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
                 </svg>
