30303f02cd387df3fce1ffcc68d2ff4bae5dfa59
danieluy <danielsosa.dev@gmail.com>
2017-06-25 00:01:30 -0300

    Refactored SVG icons

---
+++ react-app/src/assets/icons.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const CodeIcon = (props) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
+  </svg>
+)
+export const MenuIcon = (props) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+  </svg>
+)
+export const HomeIcon = (props) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+  </svg>
+)
+export const WorkIcon = (props) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z" />
+  </svg>
+)
+export const StufIcon = (props) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M13 13v8h8v-8h-8zM3 21h8v-8H3v8zM3 3v8h8V3H3zm13.66-1.31L11 7.34 16.66 13l5.66-5.66-5.66-5.65z" />
+  </svg>
+)
--- react-app/src/sections/dev/dev-navbar/DevNavbar.js
+++ react-app/src/sections/dev/dev-navbar/DevNavbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import './DevNavbar.css';
 
+import { CodeIcon, MenuIcon, HomeIcon, WorkIcon, StufIcon } from '../../../assets/icons';
+
 class DevNavbar extends Component {
 
   constructor() {
@@ -36,15 +38,9 @@ class DevNavbar extends Component {
 
           <div className="dev-nav-title">
             <Link to="/">
-              <svg className="dev-nav-title-icon" height="32" viewBox="0 0 24 24" width="32" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
-              </svg>
+              <CodeIcon className="dev-nav-title-icon" />
             </Link>
-            <svg onClick={this.openOpenCloseMenu.bind(this)} className="dev-nav-menu-icon" height="26" viewBox="0 0 24 24" width="26" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-            </svg>
+            <MenuIcon onClick={this.openOpenCloseMenu.bind(this)} className="dev-nav-menu-icon" />
             <span className="dev-nav-title-text"><i>www.danielsosa.uy</i>{window.location.pathname}</span>
           </div>
 
@@ -54,33 +50,22 @@ class DevNavbar extends Component {
             <div className="dev-nav-links">
 
               <Link to="/" className="dev-nav-mobile-back-link">
-                <svg className="dev-nav-back-icon" height="32" viewBox="0 0 24 24" width="32" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
-                </svg>
+                <CodeIcon className="dev-nav-back-icon" />
                 <span className="dev-nav-link-text dev-nav-link-text-white">wwww.danielsosa.uy</span>
               </Link>
 
               <Link to="/dev" onClick={this.openOpenCloseMenu.bind(this)} className="dev-nav-link" activeClassName="selected" onlyActiveOnIndex>
-                <svg className="dev-nav-link-icon" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                  <path d="M0 0h24v24H0z" fill="none" />
-                </svg>
+                <HomeIcon className="dev-nav-link-icon" />
                 <span className="dev-nav-link-text">{LANG.home}</span>
               </Link>
 
               <Link to="/dev/work" onClick={this.openOpenCloseMenu.bind(this)} className="dev-nav-link" activeClassName="selected">
-                <svg className="dev-nav-link-icon" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 0h24v24H0z" fill="none" />
-                  <path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z" />
-                </svg>
+                <WorkIcon className="dev-nav-link-icon" />
                 <span className="dev-nav-link-text">{LANG.work}</span>
               </Link>
 
               <Link to="/dev/stuff" onClick={this.openOpenCloseMenu.bind(this)} className="dev-nav-link" activeClassName="selected">
-                <svg className="dev-nav-link-icon" height="24" viewBox="0 0 24 24" width="24">
-                  <path d="M13 13v8h8v-8h-8zM3 21h8v-8H3v8zM3 3v8h8V3H3zm13.66-1.31L11 7.34 16.66 13l5.66-5.66-5.66-5.65z"/>
-                </svg>
+                <StufIcon className="dev-nav-link-icon" />
                 <span className="dev-nav-link-text">{LANG.stuff}</span>
               </Link>
 
